91f16b53f2579d109d47203e02b032f232bbf6d7
Nischit Shetty <njmshetty@gmail.com>
2020-12-15 05:42:27 +0530

    made image responsive

--- app.js
+++ app.js
@@ -6,21 +6,23 @@ var serverUrl = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationUrl(input){
     return serverUrl+"?text="+input;
-};
+}
+
+function showTranslation(json){
+    outputtext.innerText=json.contents.translated;
+}
 
 function errorHandler(error){
     alert("Please try again! /n ERROR DETAILS : " + error)
-};
+}
 
 function clickHandler(){
     var textInput = inputtext.value;
 
     fetch(getTranslationUrl(textInput))
     .then(response => response.json())
-    .then(json =>{ 
-        var translatedText = json.contents.translated;
-        outputtext.innerText = translatedText})
+    .then(json =>showTranslation(json))
     .catch(errorHandler)
 };
-
+console.log(clickHandler);
 btnTranslate.addEventListener("click", clickHandler);
